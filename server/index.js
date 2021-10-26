a771b7861e7a3c3c730c3bcfb2169bbe5f04649f
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-10-25 23:02:30 -0400

    Started to implement server route and model for relatedIds API.

--- server/index.js
+++ server/index.js
@@ -2,21 +2,17 @@ const express = require('express');
 const service = express();
 const port = process.env.port || 3000;
 const bodyParser = require("body-parser");
-const multer  = require('multer')
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });//this is storing in memory
-const initRoute = require('./router/index');
-// const parsedCSV = require('./middleware/csvparser.js');
-//const connection = require('./../db/pg.js');
+const routes = require('./router/index');
 const pool = require('./../db/pg.js');
 
 service.use(bodyParser.urlencoded({ extended: false }));
 service.use(bodyParser.json());
 
-service.use('/index', initRoute)
+// service.use('/index', initRoute)
 service.get('/', (req, res) => {
   res.send('Hello World!')
 })
+service.use('/products', routes)
 
 service.listen(port, err => {
   if (err)
--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,48 @@
 const express = require('express');
 let router = express.Router();
-
+//dont forget to call next()
+// router.param('//?page=1&count=5', function (req, res, next) {
+//
+//   next()
+// })
 router
-  .route('/test')
+  .route('/products')
+  //if params 'page' or 'count' exist
+  // else default to 1 and 5.
   .get((req, res) => {
+    console.log('req.params', req.params)
+    console.log('req.path', req.path)
+    // DB req
     res.send('Hi from router/index.js')
   })
-  .post((req, res) => {})
+
+router
+  .route('/products/:product_id')
+  .get((req, res) => {
+    console.log('req.params', req.params)
+    console.log('req.path', req.path)
+        // DB req
+
+    res.send('Hi from router/index.js')
+  })
+
+router
+  .route('/products/:product_id/styles')
+  .get((req, res) => {
+    console.log('req.params', req.params)
+    console.log('req.path', req.path)
+        // DB req
+
+    res.send('Hi from router/index.js')
+  })
+
+router
+  .route('/products/:product_id/related')
+  .get((req, res) => {
+  //query the db
+  //select related_product_ids from relateds where related_ids = req.param[productID]
+  //send
+  res.send('Hello')
+  })
 
 module.exports = router;
