c3557b68a73e7c0a686a0bfe9fbb4511afbdaf6e
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-11-01 21:14:20 -0400

    Added k6, dotenv, node-env-run

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const service = require('./service');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 service.listen(port, err => {
   if (err)
