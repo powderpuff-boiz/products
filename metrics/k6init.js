a0896a5bb17f6d3b1fa70e529660b05c3a5e78c8
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-11-09 20:23:40 -0500

    Fixed the '/:productId' route.

--- metrics/k6init.js
+++ metrics/k6init.js
@@ -1,20 +1,39 @@
 import http from "k6/http";
+import { sleep } from 'k6';
 
-// GET
 export let options = {
-  scenarios: {
-    constant_request_rate: {
-      executor: "constant-arrival-rate",
-      rate: 1000,
-      timeUnit: "1s",
-      duration: "30s",
-      preAllocatedVUs: 100,
-      maxVUs: 200,
-    },
-  },
+
+  // scenarios: {
+  //   constant_request_rate: {
+  //     executor: "constant-arrival-rate",
+  //     rate: 100,
+  //     timeUnit: "1s",
+  //     duration: "30s",
+  //     preAllocatedVUs: 100,
+  //     maxVUs: 200,
+  //   },
+  // },
+  stages: [
+    // { duration: '2m', target: 100 }, // below normal load
+    // { duration: '5m', target: 100 },
+    { duration: '1m', target: 200 }, // normal load
+    { duration: '2m', target: 200 },
+    { duration: '1m', target: 300 }, // around the breaking point
+    { duration: '2m', target: 300 },
+    { duration: '1m', target: 300 }, // beyond the breaking point
+    { duration: '2m', target: 400 },
+    { duration: '2m', target: 0 }, // scale down. Recovery stage.
+  ],
 };
 export default function () {
-  let rndId = Math.floor(Math.random() * (10000000 - 9000000) + 9000000);
-  const BASE_URL = `http://localhost:3002/products/`;
-  http.get(`${BASE_URL}`);
+  let rndId = Math.floor(Math.random() * 9000000); //9,069,286
+  const BASE_URL = `http://localhost:3002/products`;
+  http.get(`${BASE_URL}/${rndId}/products`);
+  // const responses = http.batch([
+  //   ['GET', `${BASE_URL}/`],
+  //   ['GET', `${BASE_URL}/${rndId}`],
+  //   ['GET', `${BASE_URL}/${rndId}/products`],
+  //   ['GET', `${BASE_URL}/${rndId}/styles`],
+  //]);
+  sleep(1);
 }
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,6 @@
 const db = require('../../db/pg.js')
 const { getRelateds, getProducts, getProductById, getStyles } = require('../../db/models');
 
-//This file's funcs intereact with the db
-
 const relateds = {
   get: (req, res) => {
     const id = req.params.product_id;
@@ -51,17 +49,19 @@ const products = {
 const ids = {
   get: (req, res) => {
     let id = req.params.product_id;
-    console.log('id is', id);
+    // console.log('id is', id);
     getProductById.getFeaturesList(id, (err, data) => {
-      if (err) {
+      // if data is an emptry array that means the product ID was not found in the DB
+      if (err || !data) {
         console.error('ERROR', err);
         res.status(404);
-        res.send(err);
-      };
-      res.status(200);
-      let features = composeIdAPI(data);
-      // console.log('featureAPI', features);
-      res.send(features);
+        res.send();
+      } else {
+        // console.log('featureAPI', data);
+        let features = composeIdAPI(data);
+        res.status(200);
+        res.send(features);
+      }
     })
   }
 }
